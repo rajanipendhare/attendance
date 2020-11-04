e63856bf17aa1890ed44cb8bb7929e0ec04c575b
rajanipendhare <rbwakchaure15@gmail.com>
2020-11-04 23:57:29 +0530

    yarn add bootstrap@4.3.1 jquery popper.js

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,57 @@
 class StudentsController < ApplicationController
+  before_action :find_student, only: %i[edit update destroy]
+  before_action :build_student, only: %i[new create]
+
   def index
+    @students = Student.all
   end
 
-  def new
+  def new; end
+
+  def edit; end
+
+  def show; end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      redirect_to students_path
+      flash[:success] = 'Student Successfully Created'
+    else
+      render :new
+      flash[:notice] = 'Student Not Create!'
+    end
   end
 
-  def edit
+  def update
+    if @student.update_attributes(student_params)
+      redirect_to students_path
+      flash[:success] = 'Student Successfully Update'
+    else
+      render :edit
+      flash[:notice] = 'Student Not Update!'
+    end
+  end
+  def destroy
+  	if @student.delete
+      redirect_to students_path
+      flash[:success] = 'Student Successfully Deleted'
+    else
+      flash[:notice] = 'Student Not Deleted!'
+    end
   end
 
-  def show
+  private
+
+  def find_student
+    @student = Student.find(params[:id])
+  end
+
+  def build_student
+    @student = Student.new
+  end
+
+  def student_params
+    params.require(:student).permit(:first_name, :last_name, :gender, :birth_date, :contact, :address, :standard, :is_active)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # root 'students#index'
   root 'students#index'
-  get 'students/new'
-  get 'students/edit'
-  get 'students/show'
+  resources :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
